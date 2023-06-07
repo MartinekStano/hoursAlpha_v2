8a2dc61e1bba1c69ce50e4596eb902fceb184736
Martinekk <martinmatostano@gmail.com>
2023-06-07 21:51:12 +0200

    add function for login

---
+++ src/main/java/sk/posam/hoursalpha/security/EmployeeDetailsService.java
@@ -0,0 +1,38 @@
+package sk.posam.hoursalpha.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import sk.posam.hoursalpha.domain.Employee;
+import sk.posam.hoursalpha.domain.repository.IEmployeeRepository;
+
+import java.util.Collections;
+import java.util.Optional;
+
+@Service
+public class EmployeeDetailsService implements UserDetailsService {
+
+    @Autowired
+    private IEmployeeRepository repository;
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<Employee> employee = repository.findEmployeeByEmail(email);
+
+        if(employee.isPresent()){
+            return new org.springframework.security.core.userdetails.User(
+                    employee.get().getEmail(),
+                    employee.get().getPassword(),
+                    true,
+                    true,
+                    true,
+                    true,
+                    Collections.singleton(new SimpleGrantedAuthority("USER"))
+            );
+        }else {
+            throw new UsernameNotFoundException("User was not found with username: " + email);
+        }
+    }
+}
