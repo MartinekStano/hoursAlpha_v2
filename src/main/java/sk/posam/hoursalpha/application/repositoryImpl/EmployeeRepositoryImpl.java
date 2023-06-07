9155c9972b5f7e053310c68c58060ac969de3095
Martinekk <martinmatostano@gmail.com>
2023-06-07 21:36:21 +0200

    add implementation for employee repository

---
+++ src/main/java/sk/posam/hoursalpha/application/repositoryImpl/EmployeeRepositoryImpl.java
@@ -0,0 +1,21 @@
+package sk.posam.hoursalpha.application.repositoryImpl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Repository;
+import sk.posam.hoursalpha.application.repositoryCrud.IEmployeeJpaRepository;
+import sk.posam.hoursalpha.domain.Employee;
+import sk.posam.hoursalpha.domain.repository.IEmployeeRepository;
+
+import java.util.Optional;
+
+@Repository
+public class EmployeeRepositoryImpl implements IEmployeeRepository {
+
+    @Autowired
+    private IEmployeeJpaRepository repository;
+    @Override
+    public Optional<Employee> findEmployeeByEmail(String email) {
+        return repository.findEmployeeByEmail(email);
+    }
+}
